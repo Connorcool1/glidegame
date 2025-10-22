46a7d475e092d3c1fb91f6bac338637db6e95827
Connor Malcolm <connor1.mlc@gmail.com>
2025-10-22 22:42:25 +0100

    ohh a lot

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # glidegame
 browser game using a glide mechanic made with javascript and html canvas
+
+# TODO
+acually make it glide
+add tail
+add a map
--- game/game.js
+++ game/game.js
@@ -4,6 +4,17 @@ var interval = setInterval(updateGameArea, 20);
 function clear() {
     ctx.clearRect(0, 0, c.width, c.height);
 }
+const maxVel = 20;
+const gravity = 0.3;
+var moveSpeed = 10;
+var airSpeed = 0.05;
+var decayX = 0.98;
+var glideMomentum = 0;
+
+let moveR = false;
+let moveL = false;
+let jump = false;
+var jumpSpeed = -20;
 
 // const input = document.querySelector("input");
 // if (input)
@@ -16,16 +27,16 @@ function logKey(e) {
 
     if (keyLog.length > 1000) keyLog.shift();
 
+    // Process player inputs 
     if (e.type === "keydown") {
-        if (e.code === "ArrowRight") {
-            playerPiece.speedX = 1;
-        } else if (e.code === "ArrowLeft") {
-            playerPiece.speedX = -1;
-        }
+        if (e.code === "KeyD") moveR = true;
+        else if (e.code === "KeyA") moveL = true;
+        else if (e.code === "KeyW") jump = true;
+
     } else if (e.type === "keyup") {
-        if (e.code === "ArrowRight" || e.code === "ArrowLeft") {
-            playerPiece.speedX = 0;
-        }
+        if (e.code === "KeyD") moveR = false;
+        else if (e.code === "KeyA") moveL = false;
+        else if (e.code === "KeyW") jump = false;
     }
 }
 window.addEventListener("keydown", logKey);
@@ -40,6 +51,7 @@ function startGame() {
 }
 
 function Player(x, y, width, height) {
+    this.onGround = false;
     this.width = width;
     this.height = height;
     this.radius = height / 2;
@@ -48,12 +60,43 @@ function Player(x, y, width, height) {
     this.x = x;
     this.y = y;
 
+    // Player updates
     this.update = function() {
+        // Draw player
         ctx.beginPath();
         ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
         ctx.fill();
     }
     this.updatePos = function() {
+        // Gravity
+        if (this.speedY > maxVel) this.speedY = maxVel;
+        else this.speedY += gravity - (glideMomentum);
+        glideMomentum *= 0.8;
+
+        // Player input movement
+        if (moveR) {
+            glideMomentum = (Math.abs(this.speedX) + this.speedY) * 0.07;
+            // Change speed based on if on ground or in air
+            if (onGround) this.speedX = moveSpeed; 
+            else if (this.speedX < moveSpeed) this.speedX += airSpeed;
+        }
+        else if (moveL) {
+            if (onGround) this.speedX = -moveSpeed;
+            else if (this.speedX > -moveSpeed) this.speedX += -airSpeed;
+        }
+        // if no movement inputs
+        else {
+            glideMomentum = 0;
+            if (Math.abs(this.speedX) > decayX) {
+                this.speedX *= decayX;
+            } else this.speedX = 0;
+        }
+
+        if (jump && onGround) {
+            this.speedY = jumpSpeed;
+            jump = false;
+        }
+
         this.x += this.speedX;
         this.y += this.speedY;
         this.collision();
@@ -62,7 +105,10 @@ function Player(x, y, width, height) {
         var bottom = c.height;
         if (this.y + this.height > bottom) {
             this.y = bottom - this.height
+            this.speedX = 0;
+            onGround = true;
         }
+        else onGround = false;
     }
 };
 
