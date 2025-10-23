44ceb16f43a21f47e290ff6f15cc4d0e10bc7f8c
Connor <connor1.mlc@gmail.com>
2025-10-23 22:58:36 +0100

    he glides

--- game/game.js
+++ game/game.js
@@ -7,9 +7,8 @@ function clear() {
 const maxVel = 20;
 const gravity = 0.3;
 var moveSpeed = 10;
-var airSpeed = 0.05;
+var airSpeed = 5;
 var decayX = 0.98;
-var glideMomentum = 0;
 
 let moveR = false;
 let moveL = false;
@@ -51,14 +50,29 @@ function startGame() {
 }
 
 function Player(x, y, width, height) {
+    const turnRate = 0.06; //radians per frame
     this.onGround = false;
     this.width = width;
     this.height = height;
     this.radius = height / 2;
     this.speedX = 0;
-    this.speedY = 1;
+    this.speedY = 0;
+    this.speed = 0; //speed total
+    this.angle = Math.PI / 2; //direction of movement
     this.x = x;
     this.y = y;
+
+    this.setPolar = function(speedX, speedY) {
+        this.speed = Math.hypot(this.speedX, this.speedY);
+        this.angle = Math.atan2(this.speedY, this.speedX); 
+    }
+
+    this.getSpeedX = function() {
+        this.speedX = this.speed * Math.cos(this.angle);
+    }
+    this.getSpeedY = function() {
+        this.speedY = this.speed * Math.sin(this.angle);
+    }
 
     // Player updates
     this.update = function() {
@@ -66,27 +80,36 @@ function Player(x, y, width, height) {
         ctx.beginPath();
         ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
         ctx.fill();
+
+        // Draw direction line
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x + (10 * Math.cos(this.angle)), this.y + (10 * Math.sin(this.angle)));
+        ctx.stroke();
     }
     this.updatePos = function() {
+        this.getSpeedX();
+        this.getSpeedY();
         // Gravity
         if (this.speedY > maxVel) this.speedY = maxVel;
-        else this.speedY += gravity - (glideMomentum);
-        glideMomentum *= 0.8;
+        else this.speedY += gravity;
+
+        this.setPolar(this.speedX, this.speedY);
+
 
         // Player input movement
         if (moveR) {
-            glideMomentum = (Math.abs(this.speedX) + this.speedY) * 0.07;
-            // Change speed based on if on ground or in air
-            if (onGround) this.speedX = moveSpeed; 
-            else if (this.speedX < moveSpeed) this.speedX += airSpeed;
+            if (!onGround) this.angle -= turnRate;
+            this.getSpeedX();
+            this.getSpeedY();
         }
         else if (moveL) {
-            if (onGround) this.speedX = -moveSpeed;
-            else if (this.speedX > -moveSpeed) this.speedX += -airSpeed;
+            if (!onGround) this.angle += turnRate;
+            this.getSpeedX();
+            this.getSpeedY();
         }
         // if no movement inputs
         else {
-            glideMomentum = 0;
             if (Math.abs(this.speedX) > decayX) {
                 this.speedX *= decayX;
             } else this.speedX = 0;
@@ -96,6 +119,13 @@ function Player(x, y, width, height) {
             this.speedY = jumpSpeed;
             jump = false;
         }
+        this.setPolar(this.speedX, this.speedY);
+
+        this.getSpeedY();
+        this.getSpeedX();
+
+        console.log("Total speed: " + this.speed + " SpeedX: " + this.speedX + " SpeedY: " + this.speedY + " Angle: " + this.angle);
+
 
         this.x += this.speedX;
         this.y += this.speedY;
